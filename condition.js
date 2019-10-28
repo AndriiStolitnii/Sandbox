9fe66fb5826491eb61eb63963f7d683fa743cfc2
Dmitiy.sid <dmitriy.sid8@gmail.com>
2019-10-28 20:05:08 +0200

    my first commit

---
+++ condition.js
@@ -0,0 +1,24 @@
+var msg = 'INFO';
+var notification = document.querySelector('.notification');
+
+//var info = 'INFO';
+    warning = 'WARN';
+    error = 'ERROR';
+    confirm - 'CONFIRM';
+
+if (msg === info) {
+    notification.classList.add('info');
+    notification.innertext = 'info'
+} else if (msg === warning){
+    notification.classList.add('warning');
+    notification.innertext = 'warning'
+}
+else if (msg === error){
+    notification.classList.add('error');
+    notification.innertext = 'error'
+}else if (msg === confirm){
+    notification.classList.add('confirm');
+    notification.innertext = 'confirm'
+} else {
+    notification.innertext = 'default log'
+}
